e5fdfab87484c6a99436d7a16c48ebd9ca9318ef
hoshianaaa <hoshina54321@gmail.com>
2022-03-07 15:02:46 +0900

    AddButton

--- rosboard/html/js/viewers/Button.js
+++ rosboard/html/js/viewers/Button.js
@@ -6,45 +6,26 @@ class Button extends Viewer {
     * @override
   **/
   onCreate() {
+
+    this.card.title.text("Control Panel");
+
     this.viewer = $('<div></div>')
       .css({'font-size': '11pt'
     , "filter": "invert(100%) saturate(50%)"})
       .appendTo(this.card.content);
 
     this.joyId = "joy-" + Math.floor(Math.random()*10000);
-    this.joy = $('<div id="' + this.joyId + '"></div>')
+    this.joy = $('<div><button id=button_1>start</button></div>')
       .css({
         "height": "250px",
       })
       .appendTo(this.viewer);
 
-    var options = {
-        zone: document.getElementById(this.joyId),
-        mode: 'semi',
-        color: 'blue', 
-        size: 150,
-        catchDistance: 300,
-    };
-    var manager = nipplejs.create(options);
-    manager.on('start', function(evt, data) {
-      let joystickX = 0.0;
-      let joystickY = 0.0;
-      currentTransport.update_joy({joystickX, joystickY});
-    }).on('end', function(evt, data) {
-      let joystickX = 0.0;
-      let joystickY = 0.0;
-      currentTransport.update_joy({joystickX, joystickY});
-    }).on('move', function(evt, data) {
-      let radian = data['angle']['radian'];
-      let distance = data['distance'];
-      let joystickX = Math.max(Math.min(Math.cos(radian)/75*distance, 1), -1);
-      let joystickY = -1*Math.max(Math.min(Math.sin(radian)/75*distance , 1), -1);
-      currentTransport.update_joy({joystickX, joystickY});
-    });
+    var button = document.createElement("button");
   }
 
   onData(msg) {
-    this.card.title.text("buttons panel");
+//    this.card.title.text("buttons panel");
   }
 }
 
